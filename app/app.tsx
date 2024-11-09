9d4bea9ea5cfe5fc8bda3fb422af176b89e182c7
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2024-11-09 21:53:20 +0000

    feat save navigation state

--- app/app.tsx
+++ app/app.tsx
@@ -1,10 +1,17 @@
-import { createStaticNavigation } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Button, LayoutAnimation } from "react-native";
-import { customFontsToLoad } from "@/theme";
-import { useFonts } from "expo-font";
-import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { useAppTheme, useThemeProvider } from "@/hooks/useAppTheme";
+import { navigationStore$ } from "@/navigationStore";
+import { customFontsToLoad } from "@/theme";
+import { observer } from "@legendapp/state/react";
+import { useLogger } from "@react-navigation/devtools";
+import {
+	type StaticParamList,
+	createStaticNavigation,
+	useNavigationContainerRef,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import { Button, LayoutAnimation, Text, View } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 function HomeScreen() {
 	const { styles } = useStyles($styles);
@@ -14,17 +21,17 @@ function HomeScreen() {
 		<View style={styles.container}>
 			<Text style={styles.text}>Home Screen</Text>
 			<Button
-    onPress={() => {
-      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut); // Animate the transition
-      setThemeContextOverride(themeContext === "dark" ? "light" : "dark");
-    }}
-    title={`Switch Theme: ${themeContext}`}
-  />
+				onPress={() => {
+					LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut); // Animate the transition
+					setThemeContextOverride(themeContext === "dark" ? "light" : "dark");
+				}}
+				title={`Switch Theme: ${themeContext}`}
+			/>
 		</View>
 	);
 }
 
-const $styles = createStyleSheet(theme => ({
+const $styles = createStyleSheet((theme) => ({
 	container: {
 		flex: 1,
 		alignItems: "center",
@@ -45,9 +52,29 @@ const RootStack = createNativeStackNavigator({
 
 const Navigation = createStaticNavigation(RootStack);
 
+type RootStackParamList = StaticParamList<typeof RootStack>;
+
+declare global {
+	namespace ReactNavigation {
+		interface RootParamList extends RootStackParamList {}
+	}
+}
+
 function App() {
-	const { themeScheme, navigationTheme, setThemeContextOverride, ThemeProvider } = useThemeProvider()
+	const navigationRef = useNavigationContainerRef();
+
+	useLogger(navigationRef);
+
+	const {
+		themeScheme,
+		navigationTheme,
+		setThemeContextOverride,
+		ThemeProvider,
+	} = useThemeProvider();
+
 	const [areFontsLoaded, fontLoadError] = useFonts(customFontsToLoad);
+
+	const navigationState = navigationStore$.state.peek();
 
 	if (!areFontsLoaded && !fontLoadError) {
 		return null;
@@ -55,9 +82,14 @@ function App() {
 
 	return (
 		<ThemeProvider value={{ themeScheme, setThemeContextOverride }}>
-			<Navigation theme={navigationTheme} />
+			<Navigation
+				ref={navigationRef}
+				theme={navigationTheme}
+				initialState={navigationState}
+				onStateChange={(state) => navigationStore$.state.set(state)}
+			/>
 		</ThemeProvider>
 	);
 }
 
-export default App;
+export default observer(App);
